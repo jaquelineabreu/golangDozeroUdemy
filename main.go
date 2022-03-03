404727203f0f2a4fe5c04b796e7da2ed5426e4e0
jaqueline <jaqueline.abreu@fliper.app>
2022-03-03 05:46:41 -0300

    variaveis

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//Explicito
+	var variavel1 string = "Variável 1"
+	fmt.Println(variavel1)
+
+	//Implicito
+	variavel2 := "Variavel 2"
+	fmt.Println(variavel2)
+
+	var (
+		variavel3 string = "lala"
+		variavel4 string = "ala"
+	)
+
+	fmt.Println(variavel3, variavel4)
+
+	variavel5, variavel6 := "Var5", "var 6"
+
+	fmt.Println(variavel5, variavel6)
+
+	const constante1 string = "Constante 1"
+	fmt.Println(constante1)
+
+	variavel5, variavel6 = variavel6, variavel5
+
+	fmt.Println(variavel5, variavel6)
+}
