90229b3056b9a05e61d19965c6f7ba82306a5c25
jaqueline <jaqueline.abreu@fliper.app>
2022-03-05 11:40:56 -0300

    Funçoes com dois retornos

--- 4 - Funções/funcoes.go
+++ 4 - Funções/funcoes.go
@@ -6,13 +6,26 @@ func somar(n1 int8, n2 int8) int8 {
 	return n1 + n2
 }
 
+func calculoMatematicos(n1, n2 int8) (int8, int8) {
+	soma := n1 + n2
+	subtracao := n1 - n2
+	return soma, subtracao
+}
+
 func main() {
 	soma := somar(10, 20)
 	fmt.Println(soma)
 
-	var f = func() {
+	var f = func(txt string) string {
 		fmt.Println("Função f")
+		return txt
+
 	}
 
-	f()
+	resultado := f("Texto da função 1 ")
+	fmt.Println(resultado)
+
+	resultadoSoma, resultadoSubtracao := calculoMatematicos(10, 15)
+	fmt.Println(resultadoSoma, resultadoSubtracao)
+
 }
