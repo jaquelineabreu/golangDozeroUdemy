c677adf01bdbaa2711bcc317e1f3724223558ee5
jaqueline <jaqueline.abreu@fliper.app>
2022-03-05 11:59:38 -0300

    operadores

---
+++ 5 - Operadores/operadores.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	soma := 1 + 2
+	subtracao := 1 - 2
+	divisao := 1 / 2
+	multiplicacao := 1 * 2
+	resto := 1 % 2
+
+	fmt.Println(soma, subtracao, divisao, multiplicacao, resto)
+
+}
