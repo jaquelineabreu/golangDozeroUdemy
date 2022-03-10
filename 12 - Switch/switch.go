4001d162573dbda0393a63bd5f8726302a2bca1c
jaqueline <jaqueline.abreu@fliper.app>
2022-03-10 10:01:41 -0300

    switch finalizado

--- 12 - Switch/switch.go
+++ 12 - Switch/switch.go
@@ -26,6 +26,60 @@ func diaDaSemana(numero int) string {
 	}
 }
 
+func diaDaSemana2(numero int) string {
+	switch {
+	case numero == 1:
+		return "Domingo"
+	case numero == 2:
+		return "Segunda-Feira"
+	case numero == 3:
+		return "Terça-Feira"
+	case numero == 4:
+		return "Quarta-Feira"
+	case numero == 5:
+		return "Quinta-Feira"
+	case numero == 6:
+		return "Sexta-Feira"
+	case numero == 7:
+		return "Sábado"
+	default:
+		return "Número Inválido"
+	}
+
+}
+
+func diaDaSemana3(numero int) string {
+	var diaDaSemana string
+	switch {
+	case numero == 1:
+		diaDaSemana = "Domingo"
+		fallthrough
+	case numero == 2:
+		diaDaSemana = "Segunda-Feira"
+	case numero == 3:
+		diaDaSemana = "Terça-Feira"
+	case numero == 4:
+		diaDaSemana = "Quarta-Feira"
+	case numero == 5:
+		diaDaSemana = "Quinta-Feira"
+	case numero == 6:
+		diaDaSemana = "Sexta-Feira"
+	case numero == 7:
+		diaDaSemana = "Sábado"
+	default:
+		diaDaSemana = "Número Inválido"
+	}
+
+	return diaDaSemana
+
+}
+
 func main() {
 	fmt.Println("Switch")
+
+	dia := diaDaSemana3(3)
+	fmt.Println(dia)
+	dia = diaDaSemana3(1)
+	fmt.Println(dia)
+
 }
