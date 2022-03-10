0335e6c61495578fd988e237c6dd78bdc9aae622
jaqueline <jaqueline.abreu@fliper.app>
2022-03-10 07:04:33 -0300

    Estruturas de controle

---
+++ 11 - Estruturas de controle/estruturas-de-controle.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Estruturas de controle")
+
+	numero := 10
+
+	if numero > 15 {
+		fmt.Println("Maior que 15")
+	} else {
+		fmt.Println("Menor ou igual a 15")
+	}
+}
