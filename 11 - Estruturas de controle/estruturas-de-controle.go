9827962816217962e17d8f6d48c7c1918eb84199
jaqueline <jaqueline.abreu@fliper.app>
2022-03-10 07:32:22 -0300

    if completos

--- 11 - Estruturas de controle/estruturas-de-controle.go
+++ 11 - Estruturas de controle/estruturas-de-controle.go
@@ -5,11 +5,20 @@ import "fmt"
 func main() {
 	fmt.Println("Estruturas de controle")
 
-	numero := 10
+	numero := -5
 
+	//IF
 	if numero > 15 {
 		fmt.Println("Maior que 15")
 	} else {
 		fmt.Println("Menor ou igual a 15")
 	}
+
+	//IF INIT
+	if outroNumero := numero; outroNumero > 0 {
+		fmt.Println("Número é maior que zero")
+	} else {
+		fmt.Print("Numero é menor que zero")
+	}
+
 }
