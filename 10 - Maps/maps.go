f75c85a7be30725298f4913f302faea43df52bba
jaqueline <jaqueline.abreu@fliper.app>
2022-03-09 23:35:37 -0300

    map completo

--- 10 - Maps/maps.go
+++ 10 - Maps/maps.go
@@ -5,10 +5,37 @@ import "fmt"
 func main() {
 	fmt.Println("Maps")
 
+	//MAP NORMAL
 	usuario := map[string]string{
 		"nome":      "Pedro",
 		"sobrenome": "Silva",
 	}
+
 	fmt.Println(usuario)
+	fmt.Println(usuario["nome"])
+
+	//MAP ANINHADO
+	usuario2 := map[string]map[string]string{
+		"nome": {
+			"primeiro": " João",
+			"ultimo":   "Pedro",
+		},
+		"curso": {
+			"nome":   "Engenharia",
+			"campus": "Campus 1 ",
+		},
+	}
+
+	fmt.Println(usuario2)
+	//DELETAR MAP
+	delete(usuario2, "nome")
+	fmt.Println(usuario2)
+
+	//ADICIONAR MAP
+	usuario2["signo"] = map[string]string{
+		"nome": "Gêmeos",
+	}
+
+	fmt.Println(usuario2)
 
 }
