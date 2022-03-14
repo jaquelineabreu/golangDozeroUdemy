277b16fedfff7e7c11ded4dfb9000a8b8e02214b
jaqueline <jaqueline.abreu@fliper.app>
2022-03-14 15:49:27 -0300

    finalização defer

--- 14 - Funções Avançadas/Defer/defer.go
+++ 14 - Funções Avançadas/Defer/defer.go
@@ -1,5 +1,38 @@
 package main
 
+import "fmt"
+
+func funcao1() {
+	fmt.Println("Executando a função 1")
+}
+
+func funcao2() {
+	fmt.Println("Executando a função 2")
+}
+
+func alunoEstaAprovado(n1, n2 float32) bool {
+	defer fmt.Println("Média calculada. Resultado será retornado")
+	fmt.Println("Entrando na função para saber se o aluno esta aprovado")
+
+	media := (n1 + n2) / 2
+
+	if media >= 6 {
+
+		return true
+	}
+
+	return false
+
+}
+
 func main() {
+	// funcao1()
+	// funcao2()
+
+	// //AGORA COM DEFER
+	// defer funcao1()
+	// funcao2()
+
+	fmt.Println(alunoEstaAprovado(7, 8))
 
 }
