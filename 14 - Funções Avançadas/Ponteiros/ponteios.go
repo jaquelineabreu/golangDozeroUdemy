da456ec6a70f3020af351902e974f33a2be8b36b
jaqueline <jaqueline.abreu@fliper.app>
2022-03-16 07:45:18 -0300

    Ponteiros

---
+++ 14 - Funções Avançadas/Ponteiros/ponteios.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func inverterSinal(numero int) int {
+	return numero * -1
+}
+
+//POnteiro de int
+func inverterSinalComPonteiro(numero *int) {
+	*numero = *numero * -1
+}
+
+func main() {
+
+	numero := 20
+	numeroInvertido := inverterSinal(numero)
+	fmt.Println(numeroInvertido)
+	fmt.Println(numero)
+
+	novoNumero := 40
+	fmt.Println(novoNumero)
+	inverterSinalComPonteiro(&novoNumero)
+	fmt.Println(novoNumero)
+
+}
